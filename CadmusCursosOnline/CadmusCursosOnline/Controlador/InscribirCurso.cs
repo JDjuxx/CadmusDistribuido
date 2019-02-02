aace464f4769f74699a43985ca1ba8d68b693340
Jose <burmamachoy@tutanota.com>
2019-02-02 10:09:32 -0800

    Controlador

--- CadmusCursosOnline/CadmusCursosOnline/Controlador/InscribirCurso.cs
+++ CadmusCursosOnline/CadmusCursosOnline/Controlador/InscribirCurso.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,27 @@ namespace CadmusCursosOnline.Controlador
 {
     class InscribirCurso
     {
+
+        public String[] select(String Select)
+        {
+
+            String[] data = new String[4];
+            SqlCommand cmd = new SqlCommand();
+            Conexion conexion = new Conexion();
+            cmd.Connection = conexion.IniciarConexion();
+            cmd.CommandText = Select;
+            cmd.ExecuteNonQuery();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+
+                data[0] = Convert.ToString(dr[0]);
+                data[1] = Convert.ToString(dr[1]);
+            }
+
+            return data;
+
+        }
 
     }
 }
